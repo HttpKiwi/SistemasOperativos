275f6a3eaf659377d9aff1de5b239db9d841c5df
angelicamunoz2204 <angelica.benavides@correounivalle.edu.co>
2019-06-19 18:47:14 -0500

    io.c modificado

--- tarea_01_ostep/io.c
+++ tarea_01_ostep/io.c
@@ -5,16 +5,37 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
+#include <stdbool.h>
+
+FILE *file;
+char c;
+
+bool exists(const char *fname)
+{
+
+    if ((file = fopen(fname, "r")))
+    {
+        return true;
+    }
+    return false;
+}
 
 int main(int argc, char *argv[]) {
-    int fd = open("/tmp/file", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    assert(fd >= 0);
-    char buffer[20];
-    sprintf(buffer, "hello world\n");
-    int rc = write(fd, buffer, strlen(buffer));
-    assert(rc == (strlen(buffer)));
-    fsync(fd);
-    close(fd);
+
+    char *fname = argv[1];
+    long pos;
+
+    if(exists(fname)){
+	printf("holi, existo");
+	fseek(file,0,SEEK_END);
+	for(pos = ftell(file)-1; pos >-1; pos--){ //Desde la ultima posicion hasta la primera
+		fseek(file, pos, SEEK_SET); //Se ubica en la posicion pos
+		c = fgetc(file); //Retorna el siguiente caracter y lo guarda
+		printf("%c", c); //Se imprime
+	
+	}printf("\n");
+    } else printf("el archivo no existe");
+    fclose(file);
     return 0;
 }
 
