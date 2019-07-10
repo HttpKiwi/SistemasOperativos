abf5328d2e792283cf4a70ccdcddbf429bb99452
HttpKiwi <HttpKiwi>
2019-07-10 00:00:14 -0500

    Ejercicio 6 cap 5

---
+++ Ejercicios-Programacion-C05/Ejercicio6.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char** argv){
+	int x = 0;
+	pid_t pid;
+	pid = fork();
+	if(pid == 0){
+		printf("Hola, soy el hijo y mi id es %d\n",(int) getpid());
+	}
+	else{
+		x = waitpid(pid,NULL,WUNTRACED);
+		printf("El id del hijo que terminò es %d\n",x);
+	}
+}
