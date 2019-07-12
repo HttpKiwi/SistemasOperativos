b7ae11ab58674fa1a7a4e2850d1a6cac9998dc9f
HttpKiwi <juan.avenia@correounivalle.edu.co>
2019-07-12 12:25:45 -0500

    memory-user.c

---
+++ 2019_07_12/memory-user.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char** argv){
+
+	int bytes;
+
+	if(argc < 2){
+		printf("No se encontraron argumentos\n");
+		return 1;
+	}
+	bytes = atoi(argv[1]);
+
+	int* array = malloc((bytes*1024*1024)*(sizeof(int)));
+
+	if(array  == NULL){
+		printf("Memoria no asignada\n");
+		exit(-1);
+	}
+
+	for(int i = 0; i < bytes*1024*1024; i++){
+		array[i] = i;
+	}
+
+	sleep(10);
+
+	return 0;
+}
